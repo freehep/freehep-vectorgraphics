dde02f7e56b134b177aad4be1956db5afec2fe74
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2006-11-18 00:33:39 +0000

    Fix FVG-259

--- freehep-graphicsio-ps/src/test/java/org/freehep/graphicsio/ps/test/PSTestSuite.java
+++ freehep-graphicsio-ps/src/test/java/org/freehep/graphicsio/ps/test/PSTestSuite.java
@@ -7,17 +7,17 @@ import org.freehep.graphicsio.test.TestSuite;
 
 /**
  * @author Mark Donszelmann
- * @version $Id: freehep-graphicsio-ps/src/test/java/org/freehep/graphicsio/ps/test/PSTestSuite.java ca9d6c536849 2006/11/12 00:49:46 duns $
+ * @version $Id: freehep-graphicsio-ps/src/test/java/org/freehep/graphicsio/ps/test/PSTestSuite.java 3e3b6c4e40de 2006/11/18 00:33:39 duns $
  */
 public class PSTestSuite extends TestSuite {
-
+/*
     protected void addTests(String category, String fmt, String dir, String ext, boolean compare, Properties properties) {
         super.addTests(category, fmt, dir, ext, compare, properties);
-//        addTest(new TestCase(
-//                "org.freehep.graphicsio.ps.test.PSTestPreviewThumbnail", category, fmt,
-//                dir, ext, compare, null));
+        addTest(new TestCase(
+                "org.freehep.graphicsio.ps.test.PSTestPreviewThumbnail", category, fmt,
+                dir, ext, compare, null));
     }
-
+*/
     public static TestSuite suite() {
         PSTestSuite suite = new PSTestSuite();
         suite.addTests("PS");
