e5f70d64a6de151bdd135b1dfe689d13af18af41
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2006-11-18 00:29:37 +0000

    Fix FVG-259

--- freehep-graphicsio-pdf/src/test/java/org/freehep/graphicsio/pdf/test/PDFTestSuite.java
+++ freehep-graphicsio-pdf/src/test/java/org/freehep/graphicsio/pdf/test/PDFTestSuite.java
@@ -7,13 +7,14 @@ import org.freehep.graphicsio.test.TestSuite;
 
 /**
  * @author Mark Donszelmann
- * @version $Id: freehep-graphicsio-pdf/src/test/java/org/freehep/graphicsio/pdf/test/PDFTestSuite.java 90a9bc32896b 2006/11/11 17:09:14 duns $
+ * @version $Id: freehep-graphicsio-pdf/src/test/java/org/freehep/graphicsio/pdf/test/PDFTestSuite.java 64a68957eb04 2006/11/18 00:29:37 duns $
  */
 public class PDFTestSuite extends TestSuite {
 
+    /*
     protected void addTests(String category, String fmt, String dir, String ext, boolean compare, Properties properties) {
         super.addTests(category, fmt, dir, ext, compare, properties);
-/*
+
         addTest(new TestCase(
                 "org.freehep.graphicsio.pdf.test.PDFTestPreviewThumbnail", category, fmt, 
                 dir, ext, compare, null));
@@ -23,8 +24,8 @@ public class PDFTestSuite extends TestSuite {
         addTest(new TestCase(
                 "org.freehep.graphicsio.pdf.test.PDFTestFontType3", category, fmt,
                 dir, ext, compare, null));
-*/
     }
+ */
 
     public static TestSuite suite() {
         PDFTestSuite suite = new PDFTestSuite();
