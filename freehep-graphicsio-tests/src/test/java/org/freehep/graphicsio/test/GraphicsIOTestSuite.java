08a7ff0f19cded9ed86e2ac6669953f901d209b9
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2007-01-09 19:24:50 +0000

    Re-enabled BMP and WBMP

--- freehep-graphicsio-tests/src/test/java/org/freehep/graphicsio/test/GraphicsIOTestSuite.java
+++ freehep-graphicsio-tests/src/test/java/org/freehep/graphicsio/test/GraphicsIOTestSuite.java
@@ -6,17 +6,17 @@ import org.freehep.graphicsio.ImageConstants;
 
 /**
  * @author Mark Donszelmann
- * @version $Id: freehep-graphicsio-tests/src/test/java/org/freehep/graphicsio/test/GraphicsIOTestSuite.java d7c75c135a1d 2007/01/09 00:32:55 duns $
+ * @version $Id: freehep-graphicsio-tests/src/test/java/org/freehep/graphicsio/test/GraphicsIOTestSuite.java 6ae8e398db70 2007/01/09 19:24:50 duns $
  */
 public class GraphicsIOTestSuite extends TestSuite {
 
     public static TestSuite suite() {
         GraphicsIOTestSuite suite = new GraphicsIOTestSuite();
-//        suite.addTests(ImageConstants.BMP);
+        suite.addTests(ImageConstants.BMP);
         suite.addTests(ImageConstants.GIF);
         suite.addTests(ImageConstants.JPG);
         suite.addTests(ImageConstants.PNG);
-//        suite.addTests(ImageConstants.WBMP);
+        suite.addTests(ImageConstants.WBMP);
         return suite;
     }
 
