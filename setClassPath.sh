b52ed6cbf9ef79d1f48ab98219b303211202f0a9
Mark Donszelmann <Mark.Donszelmann@gmail.com>
2007-06-08 21:13:45 +0000

    Fixed version problems

---
+++ setClassPath.sh
@@ -0,0 +1,17 @@
+export VG=/Users/duns/svn/freehep/vectorgraphics
+export V=2.1-SNAPSHOT
+export CLASSPATH=.
+export CLASSPATH=${CLASSPATH}:${VG}/../base/freehep-io/target/freehep-io-2.0.2-SNAPSHOT.jar
+export CLASSPATH=${CLASSPATH}:${VG}/../base/freehep-export/target/freehep-export-2.0.4-SNAPSHOT.jar
+export CLASSPATH=${CLASSPATH}:${VG}/../base/freehep-swing/target/freehep-swing-2.0.3-SNAPSHOT.jar
+export CLASSPATH=${CLASSPATH}:${VG}/../base/freehep-util/target/freehep-util-2.0.2-SNAPSHOT.jar
+export CLASSPATH=${CLASSPATH}:${VG}/freehep-graphics2d/target/freehep-graphics2d-${V}.jar
+export CLASSPATH=${CLASSPATH}:${VG}/freehep-graphicsio/target/freehep-graphicsio-${V}.jar
+export CLASSPATH=${CLASSPATH}:${VG}/freehep-graphicsio-tests/target/freehep-graphicsio-tests-${V}.jar
+export CLASSPATH=${CLASSPATH}:${VG}/freehep-graphicsio-emf/target/freehep-graphicsio-emf-${V}.jar
+export CLASSPATH=${CLASSPATH}:${VG}/freehep-graphicsio-java/target/freehep-graphicsio-java-${V}.jar
+export CLASSPATH=${CLASSPATH}:${VG}/freehep-graphicsio-pdf/target/freehep-graphicsio-pdf-${V}.jar
+export CLASSPATH=${CLASSPATH}:${VG}/freehep-graphicsio-ps/target/freehep-graphicsio-ps-${V}.jar
+export CLASSPATH=${CLASSPATH}:${VG}/freehep-graphicsio-svg/target/freehep-graphicsio-svg-${V}.jar
+export CLASSPATH=${CLASSPATH}:${VG}/freehep-graphicsio-swf/target/freehep-graphicsio-swf-${V}.jar
+
